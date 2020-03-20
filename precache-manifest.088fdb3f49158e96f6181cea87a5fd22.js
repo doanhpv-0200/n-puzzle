89a02e216fdc7d1edc1757e839345bb10500362a
Pham Van Doanh <pham.van.doanh@framgia.com>
2020-03-20 16:13:10 +0700

    Updates

---
+++ precache-manifest.088fdb3f49158e96f6181cea87a5fd22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f49de72bcc0d005ef0dd5a624020a20",
+    "url": "/n-puzzle/index.html"
+  },
+  {
+    "revision": "ddf53dabe248f1c6f77c",
+    "url": "/n-puzzle/static/css/main.4b491e7e.chunk.css"
+  },
+  {
+    "revision": "73090408cc75d8b09c8d",
+    "url": "/n-puzzle/static/js/2.5d3c586c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/n-puzzle/static/js/2.5d3c586c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddf53dabe248f1c6f77c",
+    "url": "/n-puzzle/static/js/main.5357da94.chunk.js"
+  },
+  {
+    "revision": "adeed2bfb02178307338",
+    "url": "/n-puzzle/static/js/runtime-main.1ac65a35.js"
+  }
+]);
